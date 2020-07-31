704ee97e2c07a8dc06ec6f881f8acdb48833d7a6
urw7rs <urw.7rs@icloud.com>
2020-07-31 23:19:17 +0900

    remove batch norm

--- torchbeast/core/models.py
+++ torchbeast/core/models.py
@@ -313,15 +313,12 @@ class Discriminator(nn.Module):
             nn.LeakyReLU(0.2, True),
             # (ndf) x 32 x 32
             nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 2),
             nn.LeakyReLU(0.2, True),
             # (ndf*2) x 16 x 16
             nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 4),
             nn.LeakyReLU(0.2, True),
             # (ndf*4) x 8 x 8
             nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 8),
             nn.LeakyReLU(0.2, True),
             # (ndf*8) x 4 x 4
             nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
